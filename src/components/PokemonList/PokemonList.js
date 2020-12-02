fd58205fe62371103f861b439cc8cfb098895bc4
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-12-02 15:13:46 +0000

    Able to fetch data using action

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,9 +1,34 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPokemonList } from '../../redux/actions/pokemonActions';
 
 const PokemonList = () => {
+  const dispatch = useDispatch();
+  const pokemonList = useSelector(state => state.PokemonList)
+
+  useEffect(() => {
+    fetchPokemon(1)
+  }, [])
+
+  const fetchPokemon = (page = 1) => {
+    dispatch(getPokemonList(page))
+  } 
+
+  const showData = () => {
+    if (!pokemonList.pokemonData.length) {
+      return <p>Have Data</p>
+    }
+    if (pokemonList.loading) {
+      return <p>...Loading</p>
+    }
+    if (pokemonList.errorMsg) {
+      return <p>{pokemonList.errorMsg}</p>
+    }
+  }
   return (
     <div>
-      Pokemon List
+      {showData()}
     </div>
   )
 }
