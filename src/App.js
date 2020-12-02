4c99df65d192705c6aeb64dc893f5928ac661ab8
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-12-02 11:44:02 +0000

    Added routes and a redirect for error url catch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+import PokemonList from "./components/PokemonList/PokemonList";
+import Pokemon from "./components/Pokemon/Pokemon";
 
 function App() {
-  return <div className="App">Testing</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path={"/"} exact component={PokemonList} />
+        <Route path={"/pokemon/:pokemon"} exact component={Pokemon} />
+        <Redirect to={"/"} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
