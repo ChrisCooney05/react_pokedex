a31f6408ea2ca01b3b5dd37a5f3491cf3e363206
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-12-07 10:41:12 +0000

    pagination added

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPokemonList } from "../../redux/actions/pokemonActions";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 const PokemonList = (props) => {
   const [search, setSearch] = useState("");
@@ -18,6 +19,10 @@ const PokemonList = (props) => {
   };
 
   const showData = () => {
+    if (pokemonList.loading) {
+      return <p>...Loading</p>;
+    }
+
     if (pokemonList.pokemonData.length) {
       return (
         <div className="list-wrapper">
@@ -33,9 +38,6 @@ const PokemonList = (props) => {
         </div>
       );
     }
-    if (pokemonList.loading) {
-      return <p>...Loading</p>;
-    }
     if (pokemonList.errorMsg) {
       return <p>{pokemonList.errorMsg}</p>;
     }
@@ -45,11 +47,20 @@ const PokemonList = (props) => {
       <div className="search-wrapper">
         <p>Search: </p>
         <input type="text" onChange={(e) => setSearch(e.target.value)} />
+        {/* on click we go to the /pokemon route using our state as a param */}
         <button onClick={() => props.history.push(`/pokemon/${search}`)}>
           Search
         </button>
       </div>
       {showData()}
+      {pokemonList.pokemonData.length && (
+        <ReactPaginate
+          pageCount={Math.ceil(pokemonList.count / 15)}
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={1}
+          onPageChange={(data) => fetchPokemon(data.selected + 1)}
+        />
+      )}
     </div>
   );
 };
