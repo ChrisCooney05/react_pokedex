127b231fa7ee319182d73f89e0108fdef31ebb9d
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-05 10:46:52 +0000

    Changed the App.js component to test the removal of the return() statement by using () => ()

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 import PokemonList from "./components/PokemonList/PokemonList";
 import Pokemon from "./components/Pokemon/Pokemon";
 
-function App() {
-  return (
-    <div className="App">
-      <nav>
-        <NavLink to={"/"}>Home</NavLink>
-      </nav>
-      <Switch>
-        <Route path={"/"} exact component={PokemonList} />
-        <Route
-          path={"/pokemon/:pokemon"}
-          exact
-          // using render over component means I can pass through extra props if I want
-          render={(props) => <Pokemon text={"Test"} {...props} />}
-          //component={Pokemon}
-        />
-        <Redirect to={"/"} />
-      </Switch>
-    </div>
-  );
-}
+// () => {} vs () => () we can omit the use of wrapping everything in a return()
+const App = () => (
+  <div className="App">
+    <nav>
+      <NavLink to={"/"}>Home</NavLink>
+    </nav>
+    <Switch>
+      <Route path={"/"} exact component={PokemonList} />
+      <Route
+        path={"/pokemon/:pokemon"}
+        exact
+        // using render over component means I can pass through extra props if I want
+        render={(props) => <Pokemon text={"Test"} {...props} />}
+        //component={Pokemon}
+      />
+      <Redirect to={"/"} />
+    </Switch>
+  </div>
+);
 
 export default App;
