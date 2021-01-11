72e8425db6f2272d7c4bf6abb36efc3549e4e51c
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-11 10:58:28 +0000

    Added progress bar to convert numbers into stat bar

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ProgressBar = (props) => {
+  const { statNum } = props;
+  const completed = (statNum * 100) / 255;
+
+  const containerStyles = {
+    height: 20,
+    width: "100%",
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 50,
+  };
+
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    backgroundColor: "#76ee00",
+    borderRadius: "inherit",
+    textAlign: "right",
+  };
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}></div>
+    </div>
+  );
+};
